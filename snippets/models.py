3ca85bdedfb455d9885e439ffa44b249194740a0
Calvin Spealman <ironfroggy@gmail.com>
2010-03-05 08:54:31 -0500

    Add description field and default for body"

--- snippets/models.py
+++ snippets/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Snippet(models.Model):
     title = models.CharField(max_length=50, unique=True)
-    body = models.TextField()
+    description = models.CharField(max_length=200, default="")
+    body = models.TextField(default="")
 
     def __unicode__(self):
         return "Snippet '%s'" % (self.title,)
