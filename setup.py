df4cd784f033fa4ee815d5b2ff8c34796e548e06
Calvin Spealman <ironfroggy@gmail.com>
2010-03-05 08:58:56 -0500

    Make installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = "snippets",
+    version = "150",
+    description = "simple text/html snippet libraries for django",
+    author = "Calvin Spealman",
+    author_email = "ironfroggy@gmail.com",
+    url = "http://github.com/ironfroggy/django-snippets",
+    packages = ['snippets']
+)
